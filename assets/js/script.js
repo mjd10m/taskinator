d11e9bbf932e24aa3d3f68a5e18eae1cf1e86115
mjd10m <mjd10m@outlook.com>
2022-07-02 16:41:00 -0400

    add type info to task

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,23 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 function createTaskHandler(event) {
     
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    //create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task";
+
+    //create div for type info
+    var taskInfoEl = document.createElement("div")
+    taskInfoEl.className = "task-info"
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    //combine div and list item
+    listItemE1.appendChild(taskInfoEl);
+
+    //add list item to list
     tasksToDoE1.appendChild(listItemE1);
 };
 
