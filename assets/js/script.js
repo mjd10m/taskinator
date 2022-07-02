d48618bf91c1681ac31e70bd8aee270d3d0534d6
mjd10m <mjd10m@outlook.com>
2022-07-02 16:09:45 -0400

    js to add hard coded task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-function createTaskHandler() {
+function createTaskHandler(event) {
+    
+    event.preventDefault();
+    
     var listItemE1 = document.createElement("li");
-
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task";
     tasksToDoE1.appendChild(listItemE1);
 };
 
-buttonE1.addEventListener("click", createTaskHandler)
+formE1.addEventListener("submit", createTaskHandler)
