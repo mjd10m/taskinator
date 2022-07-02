75d2205e8570a4abbd629f17614eeb8823a4627b
mjd10m <mjd10m@outlook.com>
2022-07-02 17:06:18 -0400

    bug fix for adding blank tasks and reset form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-function createTaskHandler(event) {
-    
+function taskFormHandler(event) {
     event.preventDefault();
-
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //check if form filled out completly
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formE1.reset()
+
+    //package data as object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj)
+};
+
+function createTaskEl(taskDataObj) {
     
     //create list item
     var listItemE1 = document.createElement("li");
@@ -15,13 +31,13 @@ function createTaskHandler(event) {
     //create div for type info
     var taskInfoEl = document.createElement("div")
     taskInfoEl.className = "task-info"
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     
     //combine div and list item
     listItemE1.appendChild(taskInfoEl);
 
     //add list item to list
     tasksToDoE1.appendChild(listItemE1);
-};
+}
 
-formE1.addEventListener("submit", createTaskHandler)
+formE1.addEventListener("submit", taskFormHandler)
